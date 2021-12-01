d077d63f579b9f43371889025719650c28db65c7
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2021-12-01 00:58:19 -0500

    1575rt 2

--- 01/solution.rb
+++ 01/solution.rb
@@ -17,3 +17,30 @@ end
 
 puts "increments count is #{increments}"
 
+# part 2
+#
+# sliding 3 scan window
+
+puts "part 2"
+
+increments = 0
+
+last_window_sum = nil
+
+windows = depths.length
+
+(0..(windows - 1)).each do |index|
+  last = index + 2
+
+  next if depths[last].nil?
+
+  window_sum = depths[index..last].sum
+
+  if last_window_sum && window_sum > last_window_sum
+    increments += 1
+  end
+
+  last_window_sum = window_sum
+end
+
+puts "increments count for window scan is #{increments}"
