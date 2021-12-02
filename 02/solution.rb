51a5601f9f41fa8b25097735a0294d8f6d89b826
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2021-12-02 00:05:40 -0500

    Part 1

---
+++ 02/solution.rb
@@ -0,0 +1,26 @@
+# part 1
+#
+
+instructions = File.read("input.txt").split("\n")
+
+depth = 0
+horizontal = 0
+
+instructions.each do |instruction|
+  direction, number = instruction.split(" ")
+
+  number = number.to_i
+
+  case direction
+  when "forward"
+    horizontal += number
+  when "down"
+    depth += number
+  when "up"
+    depth -= number
+  end
+end
+
+puts "depth is #{depth}. horizontal is #{horizontal}. product is:"
+
+puts horizontal * depth
