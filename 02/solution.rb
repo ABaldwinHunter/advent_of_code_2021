0b73c0dd17b19a98eb0b19bb6c2acff7ff4773e4
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2021-12-02 00:11:57 -0500

    Part 2

--- 02/solution.rb
+++ 02/solution.rb
@@ -24,3 +24,30 @@ end
 puts "depth is #{depth}. horizontal is #{horizontal}. product is:"
 
 puts horizontal * depth
+
+# part 2
+#
+
+depth = 0
+horizontal = 0
+aim = 0
+
+instructions.each do |instruction|
+  direction, number = instruction.split(" ")
+
+  number = number.to_i
+
+  case direction
+  when "forward"
+    depth += (aim * number)
+    horizontal += number
+  when "down"
+    aim += number
+  when "up"
+    aim -= number
+  end
+end
+
+puts "depth is #{depth}. horizontal is #{horizontal}. product is:"
+
+puts horizontal * depth
