793bbcb4c92f9356bbf4faec5e4733f619fb3ec5
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2021-12-08 00:24:12 -0500

    Hashesh and lantern fish

--- 06/solution.rb
+++ 06/solution.rb
@@ -1,39 +1,55 @@
+require 'pry'
+
 # Lanternfish
 #
 # each lantern fish creates a new lanernfish on ce every 7 days
+
+# # file = "input.txt"
+# file = "sample.txt"
+
+# fishes = File.read(file).split(",").map(&:to_i)
+
+# day_count = 0
+
+# new_fish_to_add = 0
+
+# while day_count < 80
+#   fishes.each.with_index do |fish, index|
+#     if fish == 0
+#       fishes[index] = 6
+#       new_fish_to_add += 1
+#     else
+#       fishes[index] = fish - 1
+#     end
+#   end
+
+#   new_fish_to_add.times do
+#     fishes << 8
+#   end
+
+#   day_count += 1
+#   new_fish_to_add = 0
+# end
+
+# puts "total fish count is #{fishes.count}"
 
 file = "input.txt"
 # file = "sample.txt"
 
 fishes = File.read(file).split(",").map(&:to_i)
-
-day_count = 0
-
-new_fish_to_add = 0
-
-while day_count < 80
-  fishes.each.with_index do |fish, index|
-    if fish == 0
-      fishes[index] = 6
-      new_fish_to_add += 1
-    else
-      fishes[index] = fish - 1
-    end
-  end
-
-  new_fish_to_add.times do
-    fishes << 8
-  end
-
-  day_count += 1
-  new_fish_to_add = 0
-end
-
-puts "total fish count is #{fishes.count}"
-
 # part 2
 
-start_fish_hash = {}
+start_fish_hash = {
+  0 => 0,
+  1 => 0,
+  2 => 0,
+  3 => 0,
+  4 => 0,
+  5 => 0,
+  6 => 0,
+  7 => 0,
+  8 => 0,
+}
 
 fishes.each do |fish|
   if start_fish_hash[fish]
@@ -43,14 +59,9 @@ fishes.each do |fish|
   end
 end
 
-target = 256
-# target = 80
-
-days = 0
-
 def evolve(fish_hash, cycles_left)
   if cycles_left == 0
-    fish_hash.map { |k, v| v }.sum
+    fish_hash.map { |k, v| v }.compact.sum
   else
     new_fish_hash = {}
 
@@ -60,6 +71,7 @@ def evolve(fish_hash, cycles_left)
     end
 
     new_fish_hash[8] = fish_hash[0]
+    new_fish_hash[6] += fish_hash[0]
 
     evolve(new_fish_hash, (cycles_left - 1))
   end
