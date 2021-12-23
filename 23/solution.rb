f74269463ed2902e13504348e548456852d1c956
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2021-12-23 01:17:58 -0500

    Day 23 part 1

---
+++ 23/solution.rb
@@ -0,0 +1,24 @@
+# similar to towers of hanoi?
+#
+##############
+#...........#
+###D#C#A#B###
+  #D#C#B#A#
+  #########
+#
+#
+# A up + left 5 - 6
+# B up 2, right 1 - 3
+# C - 500 * 2
+# B - 5
+# B - 6
+# A - 3
+# D 9000s
+# A - 9
+# A - 2
+
+# eye balling, I think trying to move the As, then the Bs make sense
+#
+# the minimum D will be is 900 each
+# C 500 each
+#
